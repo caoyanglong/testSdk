56272fad20d10009e8148f59681b33fabfa413f7
caoyanglong <caoyanglong@yyhudong.com>
2017-07-25 18:57:27 +0800

    init

---
+++ app/src/main/java/com/yyhd/fusionads/demo/TestActivity.java
@@ -0,0 +1,8 @@
+package com.yyhd.fusionads.demo;
+
+/**
+ * Created by caoyanglong on 2017/7/25.
+ */
+
+public class TestActivity {
+}
---
+++ dep/CommonUtils/src/main/java/net/lingala/MySdkUtils.java
@@ -0,0 +1,11 @@
+package net.lingala;
+
+/**
+ * Created by caoyanglong on 2017/7/25.
+ */
+
+public class MySdkUtils {
+    public static int sum(int a ,int b){
+        return a + b;
+    }
+}
