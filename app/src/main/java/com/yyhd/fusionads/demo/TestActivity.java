b416ee33a86827d957a696074f3c2a5d94fcce2c
caoyanglong <caoyanglong@yyhudong.com>
2017-07-26 10:37:43 +0800

    修改libraryName

--- app/src/main/java/com/yyhd/fusionads/demo/TestActivity.java
+++ app/src/main/java/com/yyhd/fusionads/demo/TestActivity.java
@@ -1,8 +1,20 @@
 package com.yyhd.fusionads.demo;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import net.lingala.MySdkUtils;
+
 /**
  * Created by caoyanglong on 2017/7/25.
  */
 
-public class TestActivity {
+public class TestActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        int sum = MySdkUtils.sum(1,9);
+        Toast.makeText(this, String.valueOf(sum),Toast.LENGTH_LONG).show();
+    }
 }
